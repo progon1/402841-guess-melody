5f1440c63aa92873005af1fb32a8f455dd1db704
Victor Progony <progon99@gmail.com>
2017-05-31 09:10:57 +0300

    change the element of the order array from the class value to the ordinal number screen

--- js/main.js
+++ js/main.js
@@ -1,37 +1,16 @@
 const template = document.querySelector(`#templates`);
-let screens = template.content.querySelectorAll(`section.main`);
+const screens = template.content.querySelectorAll(`section.main`);
 const target = document.querySelector(`.app .main`);
 const LEFT_ARROW = 37;
 const RIGHT_ARROW = 39;
-const mainOrder = [`main--welcome`, `main--level-genre`, `main--level-artist`, `main--result`, `main--result`];
+const order = [4, 0, 3, 2, 1];
 
-
-const sortScreens = function (order, ...pScreens) {
-  const arr = [];
-
-  for (let i = 0; i < order.length; i++) {
-    const idx = pScreens.findIndex((item) => {
-      return item.classList.contains(order[i]);
-    });
-    const element = pScreens.splice(idx, 1);
-    arr.push(...element);
-  }
-
-  if (arr[3].children.length < arr[4].children.length) {
-    const [,,, third, fourth] = arr;
-    arr.splice(3, 2, fourth, third);
-  }
-
-  pScreens.push(...arr);
-  return pScreens;
-};
-screens = sortScreens(mainOrder, ...screens);
 
 const showScreen = (num) => {
   if (target.children.length) {
-    target.replaceChild(screens[num], target.firstElementChild);
+    target.replaceChild(screens[order[num]], target.firstElementChild);
   } else {
-    target.appendChild(screens[num]);
+    target.appendChild(screens[order[num]]);
   }
 };
 
